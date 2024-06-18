a6a79cfd7ad508a77e9d5525f9bbee50b893214f
Mondez115 <tomas.mendez@ug.uchile.cl>
2024-06-18 19:00:12 -0400

    Modified to use the test function

--- main.cpp
+++ main.cpp
@@ -1,26 +1,10 @@
-#include "DijkstraHeap.h"
-#include "graph_generator.h"
+#include "test.h"
 
 int main(int argc, char* argv[]) {
 
 
-    auto start = high_resolution_clock::now();
+    test({10,12,14},{16,17,18,19,20,21,22});
 
-    vector<vector<pair<int, double>>> graf = generadorGrafo(pow(2,atoi(argv[1])),(pow(2,atoi(argv[2]))));
-
-    cout << "Grafo generado." << endl << endl;
-
-    cout << "Aplicando Dijkstra con heap al grafo generado" << endl;
-
-    pair<vector<int>,vector<double>> X = dijkstraHeap(graf,0);
-
-    cout << "Listo" << endl;
-
-    auto end = high_resolution_clock::now();
-
-    auto duration = duration_cast<milliseconds>(end - start);
-
-    cout << duration.count() << endl;
-
+    
     return 0;
 }
